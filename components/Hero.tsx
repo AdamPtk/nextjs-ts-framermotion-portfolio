cdba8b3491b9668964bec2c70e68792a6bfec86f
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-11-27 16:25:23 +0100

    About section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import me from '../public/me.jpg';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
 
 type Props = {};
 
 export default function Hero({}: Props) {
-  return <div>Hero</div>;
+  const [text, count] = useTypewriter({
+    words: [
+      'Hi, my name is Adam',
+      '<Guy-who-loves-Coffee />',
+      '<ButLovesToCodeMore />',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col items-center justify-center overflow-hidden">
+      <BackgroundCircles />
+      <div className="relative w-32 h-32 mb-2">
+        <Image src={me} alt="Author image" className="rounded-full" />
+      </div>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 p-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="">{text}</span>
+          <Cursor />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white text-center snap-y snap-mandatory z-0 overflow-hidden h-screen">
       <Head>
         <title>Adam&apos;s portfolio</title>
       </Head>
       <Header />
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* About */}
